aba8c0c934f6cd73e479a1cfad0fb865c1a2d76d
Jonathan Eichenhofer <eichenhofer.jonathan@gmail.com>
2023-11-28 09:41:08 +0100

    setup for eks

--- microservice-kubernetes-demo/docker-build-AWS.sh
+++ microservice-kubernetes-demo/docker-build-AWS.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 docker build --tag=microservice-kubernetes-demo-apache apache
-docker tag microservice-kubernetes-demo-apache 608842968837.dkr.ecr.eu-central-1.amazonaws.com/kubernetes-microservices:apache
-docker push 608842968837.dkr.ecr.eu-central-1.amazonaws.com/kubernetes-microservices:apache
+docker tag microservice-kubernetes-demo-apache 162892665763.dkr.ecr.eu-central-1.amazonaws.com/jonathaneichenhofer:apache
+docker push 162892665763.dkr.ecr.eu-central-1.amazonaws.com/jonathaneichenhofer:apache
 
 docker build --tag=microservice-kubernetes-demo-catalog microservice-kubernetes-demo-catalog
-docker tag microservice-kubernetes-demo-catalog 608842968837.dkr.ecr.eu-central-1.amazonaws.com/kubernetes-microservices:catalog
-docker push 608842968837.dkr.ecr.eu-central-1.amazonaws.com/kubernetes-microservices:catalog
+docker tag microservice-kubernetes-demo-catalog 162892665763.dkr.ecr.eu-central-1.amazonaws.com/jonathaneichenhofer:catalog
+docker push 162892665763.dkr.ecr.eu-central-1.amazonaws.com/jonathaneichenhofer:catalog
 
 docker build --tag=microservice-kubernetes-demo-customer microservice-kubernetes-demo-customer
-docker tag microservice-kubernetes-demo-customer 608842968837.dkr.ecr.eu-central-1.amazonaws.com/kubernetes-microservices:customer
-docker push 608842968837.dkr.ecr.eu-central-1.amazonaws.com/kubernetes-microservices:customer
+docker tag microservice-kubernetes-demo-customer 162892665763.dkr.ecr.eu-central-1.amazonaws.com/jonathaneichenhofer:customer
+docker push 162892665763.dkr.ecr.eu-central-1.amazonaws.com/jonathaneichenhofer:customer
 
 docker build --tag=microservice-kubernetes-demo-order microservice-kubernetes-demo-order
-docker tag microservice-kubernetes-demo-order 608842968837.dkr.ecr.eu-central-1.amazonaws.com/kubernetes-microservices:order
-docker push 608842968837.dkr.ecr.eu-central-1.amazonaws.com/kubernetes-microservices:order
+docker tag microservice-kubernetes-demo-order 162892665763.dkr.ecr.eu-central-1.amazonaws.com/jonathaneichenhofer:order
+docker push 162892665763.dkr.ecr.eu-central-1.amazonaws.com/jonathaneichenhofer:order
